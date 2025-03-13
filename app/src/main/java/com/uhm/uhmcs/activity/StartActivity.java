7a14a0784efdfcb433d0c3eaf0c72080b5dc1ed2
hjhother <1489453844@qq.com>
2025-03-13 12:08:42 +0800

    Initial commit

---
+++ app/src/main/java/com/uhm/uhmcs/activity/StartActivity.java
@@ -0,0 +1,16 @@
+package com.uhm.uhmcs.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.uhm.uhmcs.R;
+
+public class StartActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
