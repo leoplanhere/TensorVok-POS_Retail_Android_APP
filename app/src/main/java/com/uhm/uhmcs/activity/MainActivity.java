6ebf93fc9325564772d2fafa01ecb69b0ca25c76
码上钱来了 <412221776@qq.com>
2025-05-27 14:04:52 +0800

    多规格

--- app/src/main/java/com/uhm/uhmcs/activity/MainActivity.java
+++ app/src/main/java/com/uhm/uhmcs/activity/MainActivity.java
@@ -879,8 +879,10 @@ public class MainActivity extends Activity {
 
                     for (int i = 0; i < selectedShopList.size(); i++) {
                         GrouponGoodsBean.GrouponGoodsModel model = selectedShopList.get(i);
-                        Log.i("ttt", ">>>>>>>>>>>>>>" + model.getId() + "<<<<<" + grouponGoodsModel.getId());
-                        if (model.getId() == grouponGoodsModel.getId()) {
+//                        Log.i("ttt", ">>>>>>>>>>>>>>" + model.getId() + "<<<<<" + grouponGoodsModel.getId());
+                        Log.i("ttt", ">>>>>>>>Ggspid>>>>>>" + model.getGgspid() + "<<<<<" + grouponGoodsModel.getGgspid());
+                        if (model.getId() == grouponGoodsModel.getId()&&model.getGgspid() == grouponGoodsModel.getGgspid()) {
+//                            if (model.getGgspid()==0||model.getGgspid()==grouponGoodsModel.getGgspid())
                             model.setShuliang(model.getShuliang() + 1);
                             BigDecimal price = new BigDecimal(model.getPrice());
                             if (!TextUtils.isEmpty(model.getDiscount())) {
@@ -1211,8 +1213,9 @@ public class MainActivity extends Activity {
 
                     for (int i = 0; i < selectedShopList.size(); i++) {
                         GrouponGoodsBean.GrouponGoodsModel model = selectedShopList.get(i);
-                        Log.i("ttt", ">>>>>>>>>>>>>>" + model.getId() + "<<<<<" + grouponGoodsModel.getId());
-                        if (model.getId() == grouponGoodsModel.getId()) {
+//                        Log.i("ttt", ">>>>>>>>>>>>>>" + model.getId() + "<<<<<" + grouponGoodsModel.getId());
+                        Log.i("ttt", ">>>>>>>>Ggspid>>>>>>" + model.getGgspid() + "<<<<<" + grouponGoodsModel.getGgspid());
+                        if (model.getId() == grouponGoodsModel.getId()&&model.getGgspid() == grouponGoodsModel.getGgspid()) {
                             model.setShuliang(model.getShuliang() + 1);
                             BigDecimal price = new BigDecimal(model.getPrice());
                             if (!TextUtils.isEmpty(model.getDiscount())) {
