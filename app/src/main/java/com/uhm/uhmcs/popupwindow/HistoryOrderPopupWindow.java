3632470157645ecec4a03aee3fc87b6a0542cd26
码上钱来了 <412221776@qq.com>
2025-06-13 14:32:49 +0800

    2025.6.13

--- app/src/main/java/com/uhm/uhmcs/popupwindow/HistoryOrderPopupWindow.java
+++ app/src/main/java/com/uhm/uhmcs/popupwindow/HistoryOrderPopupWindow.java
@@ -184,7 +184,7 @@ public class HistoryOrderPopupWindow {
                     new PaymentPopupWindow(context,historyOrderAdapter.getData().get(position).getPaymentlog()).show();
                 }
                 if (id==R.id.daying_tv){
-                    MyPrinterHelper.getInstance().asyncPrintLastOrder(context,historyOrderAdapter.getData().get(position),null);
+                    MyPrinterHelper.getInstance().asyncPrintLastOrder(context,true,historyOrderAdapter.getData().get(position),null);
                 }
             }
         });
